6c2f600564dcab2ad0bf0e11b509fe02151ad4ac
Grolla05 <fegrolla0210@gmail.com>
2026-06-18 13:24:55 -0300

    otm

--- tests/ranking-up.test.ts
+++ tests/ranking-up.test.ts
@@ -1,6 +1,6 @@
 import assert from 'assert';
-import { isRankUp, shouldShowRankUpPopup, RankType } from '../lib/utils/rankUpUtils';
-import { getRankFromScore, UserRankingData } from '../lib/utils/rankUtils';
+import { isRankUp, shouldShowRankUpPopup, RankType } from '../front/src/lib/utils/rankUpUtils';
+import { getRankFromScore, UserRankingData } from '../front/src/lib/utils/rankUtils';
 
 // A cópia local da lógica de preenchimento para teste do comportamento esperado
 function getProcessedInitialDataForTest(
